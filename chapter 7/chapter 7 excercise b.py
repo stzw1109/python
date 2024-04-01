edf18e044a5d4649ffd59c44a67c98f048d4ffdd
SAMUEL TAI ZI WEI <b022210152@student.utem.edu.my>
2024-04-01 22:00:22 +0800

    ndew

---
+++ chapter 7/chapter 7 excercise b.py
@@ -0,0 +1,4 @@
+for x in range(1,7):
+    for y in range(1,7):
+        print(x*y,"\t" , end="")
+    print()
--- chapter6/chapter 6 exercise e.py
+++ chapter6/chapter 6 exercise e.py
@@ -1,10 +1,11 @@
-numbers = [12,75,150,180,145,525,50]
-
-for x in range(0,7):
-    if numbers[x] % 5 == 0:
-        print(numbers[x])
-    elif numbers[x] > 500:
+numbers = [12, 75, 150, 180, 145, 525, 50]
+count = 0
+while count <= 6:
+    if numbers[count] % 5 == 0 and numbers[count] <= 150 and numbers[count] <= 500:
+        print(numbers[count])
+    elif numbers[count] > 150:
+        continue
+    elif numbers[count] > 500:
         print("The number is greater than 500")
         break
-    elif numbers[x] >150:
-        continue
+    count +=1
